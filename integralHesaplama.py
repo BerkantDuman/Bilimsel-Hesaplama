493494a6970d015eef5cdf731e12aeb36f763e9c
BerkantDuman <dumanberkant562@gmail.com>
2019-03-27 16:30:08 +0300

    integral hesaplama

---
+++ integralHesaplama.py
@@ -0,0 +1,32 @@
+def f(x):
+    return (x**2)
+
+n = 10 #Aralık kaça bölünsün
+
+x1 = int(input("Alt sınır = "))
+x2 = int(input("Üst sınır = "))
+h = (x2-x1)/n
+
+#Kısa kenara göre integral
+integral = 0
+for i in range(n):
+    integral += f(x1+i*h)*h
+print("Kısa kenar=",integral)
+
+#Uzun kenara göre integral
+integral = 0
+for i in range(1,n+1):
+    integral += f(x1+i*h)*h
+print("Uzun kenar=",integral)
+
+#Orta Noktaya göre integral
+integral = 0
+for i in range(n):
+    integral += f(x1+i*h+h/2)*h
+print("Orta nokta=",integral)
+
+integral = 0
+for i in range(n):
+    integral += (f(x1+i*h) + f(x1+(i+1)*h))*h/2
+print("Yamuk alanı=",integral)
+
